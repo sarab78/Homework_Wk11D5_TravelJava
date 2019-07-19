756510a82d2598eed882cfb3232d8d8154b76af2
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-07-19 17:06:17 +0100

    First MVP Completed

---
+++ src/main/java/Plane.java
@@ -0,0 +1,25 @@
+//import java.util.ArrayList;
+//
+//public class Plane {
+//
+//    private String name;
+//    private int capacity;
+//    private ArrayList<Passenger> passengers;
+//
+//
+//    public Plane(String name, int capacity) {
+//        this.name = name;
+//        this.capacity = capacity;
+//        this.passengers = new ArrayList<Passenger>();
+//
+//
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getCapacity() {
+//        return capacity;
+//    }
+//}
---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,28 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+
+
+    @Before
+    public void setUp() {
+    passenger = new Passenger("Ross", 2);
+
+    }
+
+    @Test
+    public void passengerHasName() {
+        assertEquals("Ross", passenger.getName());
+    }
+
+    @Test
+    public void passengerHasNumberOfBags(){
+
+        assertEquals(2, passenger.getBags());
+    }
+}
